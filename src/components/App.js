dc6a2e839e91fe1e8616cc4847328bd1d33be7fe
Usman <usmanatcern@gmail.com>
2020-12-17 18:07:25 +0500

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -113,7 +113,7 @@ class App extends Component {
     else if(this.state.cardsChosenId.includes(cardId)) {
       return CARD_ARRAY[cardId].img
     } else {
-      return window.location.origin + '/images/blank.png'
+      return window.location.origin + '/images/blank1.png'
     }
   }
 
@@ -179,7 +179,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="#"
@@ -191,7 +191,7 @@ class App extends Component {
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+              <small class="text-white " ><span id="account">{this.state.account}</span></small>
             </li>
           </ul>
         </nav>
